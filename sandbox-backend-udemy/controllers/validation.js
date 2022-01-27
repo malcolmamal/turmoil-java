76c991b7acf4e6603d6ced7a0c0a7c9ff6c148fc
Malcolm Reynolds <malcolm.mal.reynolds@gmail.com>
2022-01-27 18:42:53 +0100

    removed sandbox node/react

--- sandbox-backend-udemy/controllers/validation.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-export const validationApp = new express();
-
-validationApp.get('/validate', (req, res, next) => {
-
-    res.send('validated');
-});
--- sandbox-backend-udemy/temporal.js
+++
@@ -1,6 +0,0 @@
-let a = 1;
-
-if (1 === 1) {
-    console.log(a);
-    let a = 2;
-}
